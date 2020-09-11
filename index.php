b1604162fa0a487491af92e385facb6f9a4f8ee7
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-09-11 16:03:18 +0200

    solved with PHP

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+function descendingOrder(int $n): int {
+  // Replace with your code
+    $arrayFromInt = array_map('intval', str_split($n)); // integer to array
+    rsort($arrayFromInt); // reverse order of array
+    $strFromArray = implode($arrayFromInt); // array to string
+    return intval($strFromArray); // string to integer
+}
